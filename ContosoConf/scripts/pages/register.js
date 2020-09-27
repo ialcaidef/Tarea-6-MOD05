ce4bcd23ccdd5dd4c2fcf2ee3bed19af1b82b31a
Alcaide F. Inocente <ialcaidef@hotmail.com>
2020-09-27 19:41:47 +0200

    Add project files.

---
+++ ContosoConf/scripts/pages/register.js
@@ -0,0 +1,33 @@
+﻿// Get the registration <form> element from the DOM.
+const form = document.getElementById("registration-form");
+const submitButton = form.querySelector("button");
+// Get the <input> elements from the DOM.
+const passwordInput = document.getElementById("password");
+const confirmPasswordInput = document.getElementById("confirm-password");
+
+const checkPasswords = () => {
+    const passwordsMatch = passwordInput.value === confirmPasswordInput.value;
+    if (passwordsMatch) {
+        // Clear any previous error message.
+        confirmPasswordInput.setCustomValidity("");
+    } else {
+        // Setting this error message will prevent the form from being submitted.
+        confirmPasswordInput.setCustomValidity("Sus contraseñas no coinciden. Por favor, escribe la misma contraseña de nuevo.");
+    }
+};
+
+const addPasswordInputEventListeners = () => {
+    passwordInput.addEventListener("input", checkPasswords, false);
+    confirmPasswordInput.addEventListener("input", checkPasswords, false);
+};
+
+const formSubmissionAttempted = () => {
+    form.classList.add("submission-attempted");
+};
+
+const addSubmitClickEventListener = () => {
+    submitButton.addEventListener("click", formSubmissionAttempted, false);
+};
+
+addPasswordInputEventListeners();
+addSubmitClickEventListener();
---
+++ ContosoConf/scripts/pages/schedule.js
@@ -0,0 +1,104 @@
+﻿let schedules = [];
+const list = document.getElementById("schedule");
+const track1CheckBox = document.getElementById("show-track-1");
+const track2CheckBox = document.getElementById("show-track-2");
+
+const downloadSchedule = () => {
+
+    const request = new XMLHttpRequest();
+    request.open("GET", "/schedule/list", true);
+    request.onreadystatechange = function () {
+        if (request.readyState === 4) {
+            try {
+                const response = JSON.parse(request.responseText);
+                if (request.status === 200) {
+                    schedules = response.schedule;
+                    displaySchedule();
+                } else {
+                    alert(response.message);
+                }
+            } catch (exception) {
+                alert("Schedule list not available.");
+            }
+        }
+    };
+    request.send();
+}
+
+const createSessionElement = (session) => {
+    const li = document.createElement("li");
+
+    li.sessionId = session.id;
+
+    const star = document.createElement("a");
+    star.setAttribute("href", "#");
+    star.setAttribute("class", "star");
+    li.appendChild(star);
+
+    const title = document.createElement("span");
+    title.textContent = session.title;
+    li.appendChild(title);
+
+    return li;
+};
+
+const clearList = () => {
+    while (list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+}
+
+const displaySchedule = () => {
+    clearList();
+    for (let schedule of schedules) {
+        const tracks = schedule.tracks;
+        const isCurrentTrack = (track1CheckBox.checked && tracks.indexOf(1) >= 0) ||
+            (track2CheckBox.checked && tracks.indexOf(2) >= 0);
+        if (isCurrentTrack) {
+            const li = createSessionElement(schedule);
+            list.appendChild(li);
+        }
+    }
+}
+
+const saveStar = (sessionId, isStarred) => {
+    const request = new XMLHttpRequest();
+    request.open("POST", "/schedule/star/" + sessionId, true);
+
+    if (isStarred) {
+        request.onreadystatechange = function() {
+            if (request.readyState === 4 && request.status === 200) {
+                const response = JSON.parse(request.responseText);
+                if (response.starCount > 50) {
+                    alert("¡Esta sesión es muy popular! Asegúrate de llegar temprano para conseguir un asiento.");
+                }
+            }
+        };
+    }
+
+    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    const data = "starred=" + isStarred;
+    request.send(data);
+}
+
+const handleListClick = (event) => {
+    const isStarElement = event.srcElement.classList.contains("star");
+    if (isStarElement) {
+        event.preventDefault(); // Stop the browser following the clicked <a> element's href.
+
+        const listItem = event.srcElement.parentNode;
+        if (listItem.classList.contains("starred")) {
+            listItem.classList.remove("starred");
+            saveStar(listItem.sessionId, false);
+        } else {
+            listItem.classList.add("starred");
+            saveStar(listItem.sessionId, true);
+        }
+    }
+}
+
+track1CheckBox.addEventListener("click", displaySchedule, false);
+track2CheckBox.addEventListener("click", displaySchedule, false);
+list.addEventListener("click", handleListClick, false);
+
+downloadSchedule();
